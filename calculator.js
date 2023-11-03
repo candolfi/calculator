aa1fe1e4857cdbabfeecc41b319ac243827c5c34
candolfi <jcandolfi32@gmail.com>
2023-11-03 09:28:58 -0700

    Wrote basic operation functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const add = function(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+const subtract = function(num1, num2){
+    const difference = num1 - num2;
+    return difference;
+}
+
+const multiply = function(num1, num2){
+    const product = num1 * num2;
+    return product;
+}
+
+const divide = function(num1, num2){
+    const quotient = num1 / num2;
+    return quotient;
+}
+
+console.log(divide(3,4))
