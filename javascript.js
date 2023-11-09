a1b79a1cb439f3a6b78933e36b7369ce54a46305
candolfi <jcandolfi32@gmail.com>
2023-11-08 17:21:23 -0800

    Added lines to allow calculator to "calculate" if additional operators are clicked after a 2nd number has been stored

--- javascript.js
+++ javascript.js
@@ -1,90 +1,115 @@
-let displayNum = [];
-let displayNum2 = [];
+let arrayNum = [];
+let arrayNum2 = [];
+let stringNum = [];
+let stringNum2 = [];
 let displayOperator = [];
+let answer = [];
 
-let display = document.querySelector('.display');
-let operator = document.querySelectorAll('.operator')
-let num = document.querySelectorAll('.number');
-let equate = document.querySelector('.equal');
+const display = document.querySelector('.display');
+const operator = document.querySelectorAll('.operator')
+const num = document.querySelectorAll('.number');
+const equate = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
 
 getNum1(num);
 getNum2(num);
 getOperator(operator);
+equate.addEventListener('click', event => operate(Number(stringNum),Number(stringNum2),displayOperator[0]));
+clear.addEventListener('click', event => allClear());
+
+
 
 //////////////////////////////////////////////////////////////////////////
-
-function operate(num1, num2, operand) {
-    if(operand = "+") {
-        display.innerHTML = add(num1, num2);
-        return;
-    }
-}
 
 function getOperator(item) {
     item.forEach(item => {
         item.addEventListener('click', event => {
-            display.innerHTML = item.innerHTML;
+            if(arrayNum2.length !== 0){
+               display.innerHTML = operate(Number(stringNum),Number(stringNum2),displayOperator[0]);
+            }
+            // display.innerHTML = item.innerHTML;
             displayOperator = [item.innerHTML];
         });
     })
-    return displayOperator;
+    return displayOperator[0];
 }
 
 function getNum1(item) {
         item.forEach(item => {
             item.addEventListener('click', event => {
                 if(displayOperator.length === 0){
-                    displayNum.push(Number(item.innerHTML));
-                    display.innerHTML = displayNum.join("");
+                    arrayNum.push(Number(item.innerHTML))
+                    stringNum = arrayNum.join("");
+                    display.innerHTML = arrayNum.join("");
                 }
             });
     })
-    return displayNum
+    return
 }
 
 function getNum2(item) {
     item.forEach(item => {
         item.addEventListener('click', event => {
             if(displayOperator.length > 0){
-                displayNum2.push(Number(item.innerHTML));
-                display.innerHTML = displayNum2.join("");
+                arrayNum2.push(Number(item.innerHTML));
+                stringNum2 = arrayNum2.join("");
+                display.innerHTML = arrayNum2.join("");
             }
         });
     })
-    return displayNum2
+    return
 }
 
-const add = (num1, num2) => {
+function allClear() {
+    arrayNum = [];
+    arrayNum2 = [];
+    displayOperator = [];
+    answer = []
+    stringNum = [];
+    stringNum2 = [];
+    display.innerHTML = '0';
+    return;
+}
+
+function add(num1, num2){
     const sum = num1 + num2;
     return sum;
 }
 
-const subtract = function(num1, num2){
+function subtract(num1, num2){
     const difference = num1 - num2;
     return difference;
 }
 
-const multiply = function(num1, num2){
+function multiply(num1, num2){
     const product = num1 * num2;
     return product;
 }
 
-const divide = function(num1, num2){
+function divide(num1, num2){
     const quotient = num1 / num2;
     return quotient;
 }
 
-const operate = function(num1, num2, operator){
-    if(operator == '+'){
-        return add(num1,num2);
+function operate(num1, num2, operator){
+    if(operator == ' + '){ 
+        stringNum = add(num1,num2);
+        arrayNum2 = [];
+        return display.innerHTML = add(num1,num2);
     }
-    if(operator == '-'){
-        return subtract(num1,num2);
+    if(operator == ' - '){
+        stringNum = subtract(num1,num2);
+        arrayNum2 = [];
+        return display.innerHTML = subtract(num1,num2);
     }
-    if(operator == '*'){
-        return multiply(num1,num2);
+    if(operator == ' x '){
+        stringNum = multiply(num1,num2);
+        arrayNum2 = [];
+        return display.innerHTML = multiply(num1,num2);
     }
-    if(operator == '/'){
-        return divide(num1,num2);
+    if(operator == ' / '){
+        stringNum = divide(num1,num2);
+        arrayNum2 = [];
+        return display.innerHTML = divide(num1,num2);
     }
 }
